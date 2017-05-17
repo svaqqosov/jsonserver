84381a853254216988b609ed47e22062b5219ae2
Sardor <sardor@next-aeon.com>
2017-05-17 17:01:00 +0900

    Any route & any document support added

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,42 +3,49 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 
 class IndexController extends Controller
 {
-    public function index()
+    public function index($slug)
     {
-        $data = json_decode(Storage::disk('local')->get('project_'.$id.'.json'));
+        $data = [];
+        $storagePath  = Storage::disk('local')->getDriver()->getAdapter()->getPathPrefix();
+
+        $slug_files = File::glob($storagePath.$slug.'_*');
+        foreach($slug_files as $file){
+            $data[] = json_decode(file_get_contents($file));
+        }
         return response()->json($data);
     }
-    public function show($id)
+    public function show($slug, $id)
     {
-        $data = json_decode(Storage::disk('local')->get('project_'.$id.'.json'));
+        $data = json_decode(Storage::disk('local')->get($slug.'_'.$id.'.json'));
         return response()->json($data);
     }
-    public function store(Request $request){
-        Storage::disk('local')->put('project_'.$request->get('id').'.json', $request->getContent());
+    public function store(Request $request, $slug){
+        Storage::disk('local')->put($slug.'_'.$request->get('id').'.json', $request->getContent());
         return $request->all();
     }
-    public function put(Request $request, $id){
-        Storage::disk('local')->put('project_'.$id.'.json', $request->getContent());
+    public function put(Request $request, $slug, $id){
+        Storage::disk('local')->put($slug.'_'.$id.'.json', $request->getContent());
         return $request->all();
     }
-    public function patch(Request $request, $id){
+    public function patch(Request $request, $slug, $id){
         $request->expectsJson();
-        $data = json_decode(Storage::disk('local')->get('project_'.$id.'.json'));
+        $data = json_decode(Storage::disk('local')->get($slug.'_'.$id.'.json'));
 
         $object = json_decode($request->getContent());
         foreach(get_object_vars($object) as $key => $val){
             $data->$key = $val;
         }
-        Storage::disk('local')->put('project_'.$id.'.json', json_encode($data));
+        Storage::disk('local')->put($slug.'_'.$id.'.json', json_encode($data));
         return response()->json($data);
     }
-    public function delete($id){
+    public function delete($slug, $id){
         $data = json_decode(Storage::disk('local')->get('project_'.$id.'.json'));
-        Storage::disk('local')->delete('project_'.$id.'.json');
+        Storage::disk('local')->delete($slug.'_'.$id.'.json');
         return response()->json($data);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -12,10 +12,16 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('{slug}', 'IndexController@index')->where('slug', '([a-z]+)?');
+Route::get('{slug}/{id}', 'IndexController@show')->where('slug', '([a-z]+)?');
+Route::post('{slug}', 'IndexController@store')->where('slug', '([a-z]+)?');
+Route::put('{slug}/{id}', 'IndexController@put')->where('slug', '([a-z]+)?');
+Route::patch('{slug}/{id}', 'IndexController@patch')->where('slug', '([a-z]+)?');
+Route::delete('{slug}/{id}', 'IndexController@delete')->where('slug', '([a-z]+)?');
 
-Route::get('project', 'IndexController@show');
-Route::get('project/{id}', 'IndexController@show');
-Route::post('project', 'IndexController@store');
-Route::put('project/{id}', 'IndexController@put');
-Route::patch('project/{id}', 'IndexController@patch');
-Route::delete('project/{id}', 'IndexController@delete');
+//Route::get('project', 'IndexController@show');
+//Route::get('project/{id}', 'IndexController@show');
+//Route::post('project', 'IndexController@store');
+//Route::put('project/{id}', 'IndexController@put');
+//Route::patch('project/{id}', 'IndexController@patch');
+//Route::delete('project/{id}', 'IndexController@delete');
